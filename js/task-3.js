c7aa44445b54c4c6cd86651841d740cd6bc258a2
Serhii Zhelobkov <32736906+melovarr@users.noreply.github.com>
2025-01-31 10:05:34 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+function filterArray(numbers, value) {
+    const filterNumbers = [];
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > value) {
+            filterNumbers.push(numbers[index]);
+        }
+    }
+    return filterNumbers;
+}
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
